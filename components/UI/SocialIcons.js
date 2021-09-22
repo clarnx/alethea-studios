e9dfa9a2b933d9df94315fed6f3df9f6fa60628f
clarnx <clarenceonline@outlook.com>
2021-09-22 22:33:12 +0100

    added only facebook and instagram links

--- components/UI/SocialIcons.js
+++ components/UI/SocialIcons.js
@@ -1,14 +1,18 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-const SocialIcons = ({isNavMenuItem}) => {
+const SocialIcons = ({ isNavMenuItem }) => {
     return (
-        <span className={`nav-item ${isNavMenuItem ? "ms-3" : "list-unstyled"}`}>
+        <span
+            className={`nav-item ${isNavMenuItem ? "ms-3" : "list-unstyled"}`}
+        >
             <a
                 className="bi-facebook text-light px-2"
-                href="#"
+                href="https://www.facebook.com/Alethea-Studios-100475655717187"
+                target="_blank"
+                rel="noreferrer"
                 style={{ fontSize: "2rem" }}
             ></a>
-            <a
+            {/* <a
                 className="bi-twitter text-light px-2"
                 href="#"
                 style={{ fontSize: "2rem" }}
@@ -17,7 +21,7 @@ const SocialIcons = ({isNavMenuItem}) => {
                 className="bi-linkedin text-light px-2"
                 href="#"
                 style={{ fontSize: "2rem" }}
-            ></a>
+            ></a> */}
             <a
                 className="bi-instagram text-light px-2"
                 href="https://www.instagram.com/alethea_studios"
